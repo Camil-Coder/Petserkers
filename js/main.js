d451f659d9b02185af8c4e0908f73f430f07491d
Camilo Castillo <cristian.c.castillo1707@gmail.com>
2023-12-13 11:59:25 -0500

    Seleccione de enemigo aleatorio

--- js/main.js
+++ js/main.js
@@ -7,33 +7,55 @@ function iniciarJuego() {
 };
 
 function sleccionarPetserkerJugador() {
-    var PetserkerJugador = document.getElementById('PetserkerJugador');
     const cropo = document.getElementById('cropo');
     const lunetai = document.getElementById('lunetai');
     const plichustan = document.getElementById('plichustan');
     const floki = document.getElementById('floki');
     const yinyin = document.getElementById('yinyin');
     const minihen = document.getElementById('minihen');
-    const petserkers = ['cropo', 'lunetai', 'plichustan', 'floki', 'yinyin', 'minihen'];
+    const petserkerJugador = document.getElementById('PetserkerJugador');
+
     if (cropo.checked) {
-        PetserkerJugador.innerHTML= petserkers[0];
+        petserkerJugador.innerHTML = listaPetserkers(0);
     }
     else if (lunetai.checked) {
-        PetserkerJugador.innerHTML= petserkers[1];
+        petserkerJugador.innerHTML = listaPetserkers(1);
     }
     else if (plichustan.checked) {
-        PetserkerJugador.innerHTML= petserkers[2];
+        petserkerJugador.innerHTML = listaPetserkers(2);
     }
     else if (floki.checked) {
-        PetserkerJugador.innerHTML= petserkers[3];
+        petserkerJugador.innerHTML = listaPetserkers(3);
     }
     else if (yinyin.checked) {
-        PetserkerJugador.innerHTML= petserkers[4];
+        petserkerJugador.innerHTML = listaPetserkers(4);
     }
     else if (minihen.checked) {
-        PetserkerJugador.innerHTML= petserkers[5];
+        petserkerJugador.innerHTML = listaPetserkers(5);
     } else {
         alert('selecciona un petserker 🤬')
     }
+
+    seleccionPetserkerEnemigo();
 };
 
+function seleccionPetserkerEnemigo() {
+    const petserkerEnemiga = document.getElementById('PetserkerEnemiga');
+    var petserkerAleatorio = aleatorio(6, 1)
+    if (petserkerAleatorio == 6) {
+        petserkerAleatorio = 0;
+        petserkerEnemiga.innerHTML= listaPetserkers(petserkerAleatorio);
+    }else{
+        petserkerEnemiga.innerHTML= listaPetserkers(petserkerAleatorio);
+    }
+};
+
+function aleatorio(max, min) {
+    let resultado = 0;
+    return resultado = Math.floor(Math.random() * (max - min + 1) + 1);
+}
+
+function listaPetserkers(num) {
+    var petserkers = ['cropo', 'lunetai', 'plichustan', 'floki', 'yinyin', 'minihen'];
+    return petserkers[num];
+}
