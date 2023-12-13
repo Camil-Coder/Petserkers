c3d26e1d866c736c3764fa27456d2d9f05564c9e
Camilo Castillo <cristian.c.castillo1707@gmail.com>
2023-12-13 10:44:15 -0500

    Implementacion de innerHTML

--- js/main.js
+++ js/main.js
@@ -1,34 +1,39 @@
 window.addEventListener('load', iniciarJuego);//evento para escuchar cuando el navegador esta cargado al 100%
 
+
 function iniciarJuego() {
     var btnPetserker = document.getElementById('btn-petserker');
     btnPetserker.addEventListener('click', sleccionarPetserkerJugador);
 };
 
 function sleccionarPetserkerJugador() {
-    let cropo = document.getElementById('cropo');
-    let lunetai= document.getElementById('lunetai');
-    let plichustan= document.getElementById('plichustan');
-    let floki= document.getElementById('floki');
-    let yinyin= document.getElementById('yinyin');
-    let minihen= document.getElementById('minihen');
-    
-    if(cropo.checked){
-        alert('seleccionaste cropo');
+    var PetserkerJugador = document.getElementById('PetserkerJugador');
+    const cropo = document.getElementById('cropo');
+    const lunetai = document.getElementById('lunetai');
+    const plichustan = document.getElementById('plichustan');
+    const floki = document.getElementById('floki');
+    const yinyin = document.getElementById('yinyin');
+    const minihen = document.getElementById('minihen');
+    const petserkers = ['cropo', 'lunetai', 'plichustan', 'floki', 'yinyin', 'minihen'];
+    if (cropo.checked) {
+        PetserkerJugador.innerHTML= petserkers[0];
     }
-    else if (lunetai.checked){
-        alert('seleccionaste lunetai');
+    else if (lunetai.checked) {
+        PetserkerJugador.innerHTML= petserkers[1];
     }
-    else if (plichustan.checked){
-        alert('seleccionaste plichustan');
+    else if (plichustan.checked) {
+        PetserkerJugador.innerHTML= petserkers[2];
     }
-    else if (floki.checked){
-        alert('seleccionaste floki');
+    else if (floki.checked) {
+        PetserkerJugador.innerHTML= petserkers[3];
     }
-    else if (yinyin.checked){
-        alert('seleccionaste yinyin');
+    else if (yinyin.checked) {
+        PetserkerJugador.innerHTML= petserkers[4];
     }
-    else if (minihen.checked){
-    alert('seleccionaste minihen');        
+    else if (minihen.checked) {
+        PetserkerJugador.innerHTML= petserkers[5];
+    } else {
+        alert('selecciona un petserker 🤬')
     }
 };
+
