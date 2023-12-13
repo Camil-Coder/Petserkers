eb94a296e6084ce3319226a35683e4045466f94a
Camilo Castillo <cristian.c.castillo1707@gmail.com>
2023-12-13 07:55:58 -0500

    Implementacion de evento load en un addEventListener, y logica de seleccion de petserker

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
-let btnPetserker = document.getElementById('btn-petserker');
+window.addEventListener('load', iniciarJuego);//evento para escuchar cuando el navegador esta cargado al 100%
 
-function sleccionarPetserkerJugador (){
-    alert('seleccionaste tu perserker');
+function iniciarJuego() {
+    var btnPetserker = document.getElementById('btn-petserker');
+    btnPetserker.addEventListener('click', sleccionarPetserkerJugador);
 };
 
-
-btnPetserker.addEventListener('click', sleccionarPetserkerJugador);
+function sleccionarPetserkerJugador() {
+    let cropo = document.getElementById('cropo');
+    let lunetai= document.getElementById('lunetai');
+    let plichustan= document.getElementById('plichustan');
+    let floki= document.getElementById('floki');
+    let yinyin= document.getElementById('yinyin');
+    let minihen= document.getElementById('minihen');
+    
+    if(cropo.checked){
+        alert('seleccionaste cropo');
+    }
+    else if (lunetai.checked){
+        alert('seleccionaste lunetai');
+    }
+    else if (plichustan.checked){
+        alert('seleccionaste plichustan');
+    }
+    else if (floki.checked){
+        alert('seleccionaste floki');
+    }
+    else if (yinyin.checked){
+        alert('seleccionaste yinyin');
+    }
+    else if (minihen.checked){
+    alert('seleccionaste minihen');        
+    }
+};
